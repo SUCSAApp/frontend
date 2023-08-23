52764a54d6eead2fe82bec4b6fb0a1431e12918a
Yankai Chen <yche3340@uni.sydney.edu.au>
2023-08-24 01:05:00 +1000

    2

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'Homepage/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+Color myColor = const Color.fromRGBO(29,32,136,1.0);
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SUCSA',
+
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(primary: Colors.white),
+        useMaterial3: true,
+      ),
+
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/shangjia/offer_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class OfferPage extends StatefulWidget {
+  const OfferPage({super.key});
+
+  @override
+  State<OfferPage> createState() => _OfferPageState();
+}
+
+class _OfferPageState extends State<OfferPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      '商家',
+      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    );
+  }
+}
