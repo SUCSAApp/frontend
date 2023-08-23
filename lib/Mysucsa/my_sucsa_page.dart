a5c1852b8570a47729d2a036a5eb647aa0977a7e
Yankai Chen <yche3340@uni.sydney.edu.au>
2023-08-24 00:48:43 +1000

    1

---
+++ lib/Mysucsa/my_sucsa_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MySUCSAPage extends StatelessWidget {
+  const MySUCSAPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      '我的',
+      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    );
+  }
+}
